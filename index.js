eec014ca39dfdf64a1c841d9a0eef14f95be85f1
Dylan Ocampo <dylanomar@live.com.mx>
2024-05-27 11:43:33 -0600

    demo

---
+++ index.js
@@ -0,0 +1,392 @@
+'use strict';
+
+const heygen_API = {
+  apiKey: 'NjQ0NTAzODM0NjEwNDRjM2EzNGU3M2IwYzVkZWI3YTItMTcxNjQwNTY0OQ==',
+  serverUrl: 'https://api.heygen.com',
+};
+const taskInput = document.querySelector(".bar-wrapper input");
+const avatar = "";
+const voice = "";
+const apiKey = heygen_API.apiKey;
+const SERVER_URL = heygen_API.serverUrl;
+
+if (apiKey === 'YourApiKey' || SERVER_URL === '') {
+  alert('Please enter your API key and server URL in the api.json file');
+}
+
+let sessionInfo = null;
+let peerConnection = null;
+
+function updateStatus(message) {
+  console.log(message);
+}
+
+updateStatus( 'Please click the new button to create the stream first.');
+
+function onMessage(event) {
+  const message = event.data;
+  console.log('Received message:', message);
+}
+
+// Create a new WebRTC session when clicking the "New" button
+async function createNewSession() {
+  updateStatus('Creating new session... please wait');
+  // call the new interface to get the server's offer SDP and ICE server to create a new RTCPeerConnection
+  sessionInfo = await newSession('low', avatar, voice);
+  
+  const { sdp: serverSdp, ice_servers2: iceServers } = sessionInfo;
+
+  // Create a new RTCPeerConnection
+  peerConnection = new RTCPeerConnection({ iceServers: iceServers });
+
+  // When audio and video streams are received, display them in the video element
+  peerConnection.ontrack = (event) => {
+    console.log('Received the track');
+    if (event.track.kind === 'audio' || event.track.kind === 'video') {
+      mediaElement.srcObject = event.streams[0];
+    }
+  };
+
+  // When receiving a message, display it in the status element
+  peerConnection.ondatachannel = (event) => {
+    const dataChannel = event.channel;
+    dataChannel.onmessage = onMessage;
+  };
+
+  // Set server's SDP as remote description
+  const remoteDescription = new RTCSessionDescription(serverSdp);
+  await peerConnection.setRemoteDescription(remoteDescription);
+
+  updateStatus('Session creation completed');
+  updateStatus('Now.You can click the start button to start the stream');
+  startAndDisplaySession();
+}
+
+// Start session and display audio and video when clicking the "Start" button
+async function startAndDisplaySession() {
+  if (!sessionInfo) {
+    updateStatus('Please create a connection first');
+    return;
+  }
+
+  updateStatus('Starting session... please wait');
+
+  // Create and set local SDP description
+  const localDescription = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(localDescription);
+
+ // When ICE candidate is available, send to the server
+  peerConnection.onicecandidate = ({ candidate }) => {
+    console.log('Received ICE candidate:', candidate);
+    if (candidate) {
+      handleICE(sessionInfo.session_id, candidate.toJSON());
+    }
+  };
+
+  // When ICE connection state changes, display the new state
+  peerConnection.oniceconnectionstatechange = (event) => {
+    updateStatus(
+      `ICE connection state changed to: ${peerConnection.iceConnectionState}`,
+    );
+  };
+
+
+
+  // Start session
+  await startSession(sessionInfo.session_id, localDescription);
+
+  var receivers = peerConnection.getReceivers();
+
+  receivers.forEach((receiver) => {
+    receiver.jitterBufferTarget = 500
+  });
+
+   updateStatus('Session started successfully');
+}
+
+
+
+// When clicking the "Send Task" button, get the content from the input field, then send the tas
+async function repeatHandler() {
+  if (!sessionInfo) {
+    updateStatus('Please create a connection first');
+
+    return;
+  }
+  updateStatus('Sending task... please wait');
+  const text = taskInput.value;
+  if (text.trim() === '') {
+    alert('Please enter a task');
+    return;
+  }
+
+  const resp = await repeat(sessionInfo.session_id, text);
+
+  updateStatus('Task sent successfully');
+}
+
+
+async function talkHandler(message) {
+  if (!sessionInfo) {
+    updateStatus('Please create a connection first');
+    return;
+  }
+  console.log(message);
+  updateStatus('Talking to LLM... please wait');
+
+  try {
+    const text = await talkToOpenAI(message)
+
+    if (text) {
+      // Send the AI's response to Heygen's streaming.task API
+      const resp = await repeat(sessionInfo.session_id, text);
+      updateStatus('LLM response sent successfully');
+    } else {
+      updateStatus('Failed to get a response from AI');
+    }
+  } catch (error) {
+    console.error('Error talking to AI:', error);
+    updateStatus('Error talking to AI');
+  }
+}
+
+
+// when clicking the "Close" button, close the connection
+async function closeConnectionHandler() {
+  if (!sessionInfo) {
+    updateStatus('Please create a connection first');
+    return;
+  }
+
+  renderID++;
+  hideElement(canvasElement);
+  hideElement(bgCheckboxWrap);
+  mediaCanPlay = false;
+
+  updateStatus('Closing connection... please wait');
+  try {
+    // Close local connection
+    peerConnection.close();
+    // Call the close interface
+    const resp = await stopSession(sessionInfo.session_id);
+
+    console.log(resp);
+  } catch (err) {
+    console.error('Failed to close the connection:', err);
+  }
+  updateStatus('Connection closed successfully');
+}
+document.querySelector('#talkBtn').addEventListener('click', talkHandler);  // en vez de send en el otro archivo hacer esto con el boton  
+
+
+// new session
+async function newSession(quality, avatar_name, voice_id) {
+  const response = await fetch(`${SERVER_URL}/v1/streaming.new`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': apiKey,
+    },
+    body: JSON.stringify({
+      quality,
+      avatar_name,
+      voice: {
+        voice_id: voice_id,
+      },
+    }),
+  });
+  if (response.status === 500) {
+    console.error('Server error');
+    updateStatus(
+      'Server Error. Please ask the staff if the service has been turned on',
+    );
+
+    throw new Error('Server error');
+  } else {
+    const data = await response.json();
+    console.log(data.data);
+    return data.data;
+  }
+}
+
+// start the session
+async function startSession(session_id, sdp) {
+  const response = await fetch(`${SERVER_URL}/v1/streaming.start`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': apiKey,
+    },
+    body: JSON.stringify({ session_id, sdp }),
+  });
+  if (response.status === 500) {
+    console.error('Server error');
+    updateStatus(
+      'Server Error. Please ask the staff if the service has been turned on',
+    );
+    throw new Error('Server error');
+  } else {
+    const data = await response.json();
+    return data.data;
+  }
+}
+
+// submit the ICE candidate
+async function handleICE(session_id, candidate) {
+  const response = await fetch(`${SERVER_URL}/v1/streaming.ice`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': apiKey,
+    },
+    body: JSON.stringify({ session_id, candidate }),
+  });
+  if (response.status === 500) {
+    console.error('Server error');
+    updateStatus(
+      'Server Error. Please ask the staff if the service has been turned on',
+    );
+    throw new Error('Server error');
+  } else {
+    const data = await response.json();
+    return data;
+  }
+}
+
+async function talkToOpenAI(prompt) {
+  const response = await fetch(`http://localhost:3000/openai/complete`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ prompt }),
+  });
+  if (response.status === 500) {
+    console.error('Server error');
+    updateStatus(
+      'Server Error. Please make sure to set the openai api key',
+    );
+    throw new Error('Server error');
+  } else {
+    const data = await response.json();
+    return data.text;
+  }
+}
+
+// repeat the text
+async function repeat(session_id, text) {
+  const response = await fetch(`${SERVER_URL}/v1/streaming.task`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': apiKey,
+    },
+    body: JSON.stringify({ session_id, text }),
+  });
+  if (response.status === 500) {
+    console.error('Server error');
+    updateStatus(
+      'Server Error. Please ask the staff if the service has been turned on',
+    );
+    throw new Error('Server error');
+  } else {
+    const data = await response.json();
+    return data.data;
+  }
+}
+
+// stop session
+async function stopSession(session_id) {
+  const response = await fetch(`${SERVER_URL}/v1/streaming.stop`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': apiKey,
+    },
+    body: JSON.stringify({ session_id }),
+  });
+  if (response.status === 500) {
+    console.error('Server error');
+    updateStatus( 'Server Error. Please ask the staff for help');
+    throw new Error('Server error');
+  } else {
+    const data = await response.json();
+    return data.data;
+  }
+}
+
+const removeBGCheckbox = document.querySelector('#removeBGCheckbox');
+
+let renderID = 0;
+function renderCanvas() {
+  if (!removeBGCheckbox.checked) return;
+  hideElement(mediaElement);
+  showElement(canvasElement);
+
+  canvasElement.classList.add('show');
+
+  const curRenderID = Math.trunc(Math.random() * 1000000000);
+  renderID = curRenderID;
+
+  const ctx = canvasElement.getContext('2d', { willReadFrequently: true });
+
+  if (bgInput.value) {
+    canvasElement.parentElement.style.background = bgInput.value?.trim();
+  }
+
+  function processFrame() {
+    if (!removeBGCheckbox.checked) return;
+    if (curRenderID !== renderID) return;
+
+    canvasElement.width = mediaElement.videoWidth;
+    canvasElement.height = mediaElement.videoHeight;
+
+    ctx.drawImage(mediaElement, 0, 0, canvasElement.width, canvasElement.height);
+    ctx.getContextAttributes().willReadFrequently = true;
+    const imageData = ctx.getImageData(0, 0, canvasElement.width, canvasElement.height);
+    const data = imageData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+      const red = data[i];
+      const green = data[i + 1];
+      const blue = data[i + 2];
+
+      // You can implement your own logic here
+      if (isCloseToGreen([red, green, blue])) {
+        // if (isCloseToGray([red, green, blue])) {
+        data[i + 3] = 0;
+      }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+
+    requestAnimationFrame(processFrame);
+  }
+
+  processFrame();
+}
+
+function isCloseToGreen(color) {
+  const [red, green, blue] = color;
+  return green > 90 && red < 90 && blue < 90;
+}
+
+function hideElement(element) {
+  element.classList.add('hide');
+  element.classList.remove('show');
+}
+
+
+const mediaElement = document.querySelector('#mediaElement');
+let mediaCanPlay = false;
+mediaElement.onloadedmetadata = () => {
+  mediaCanPlay = true;
+  mediaElement.play();
+
+  showElement(bgCheckboxWrap);
+};
+createNewSession();
+
+const canvasElement = document.querySelector('#canvasElement');
+
+const bgCheckboxWrap = document.querySelector('#bgCheckboxWrap');
